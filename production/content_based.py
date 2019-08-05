4f55ae333db07e65f5c5a2c8c7ae54a2d69d3872
YJM <1264405781@qq.com>
2019-08-05 14:29:15 +0800

    最终版完成，无bug
    之前版本少了点东西

--- production/content_based.py
+++ production/content_based.py
@@ -17,12 +17,12 @@ def get_up(item_cate,input_file):
     """
     :param item_cate:itemid,value：dict,key category value ratio
     :param input_file:user rating file
-    :return:a dict:key userid ,value[(catrgory,ratio),(category1,ration1)]
+    :return:a dict:key userid ,value[(catrgory,ratio),(category1,ration1)] 返回的是用户对于某些种类的偏好
     """
     if not os.path.exists(input_file):
         return {}
     linenum = 0
-    record = {}
+    record ={} #记录每一个userid对于每一个类别的得分
     up={}
     topk=2
     score_thr=4.0
@@ -32,40 +32,51 @@ def get_up(item_cate,input_file):
             linenum +=1
             continue
         item = line.strip().split(",")
-        if len(item)<4:
+        if len(item) < 4:
             continue
+
         userid,itemid,rating,timestamp = item[0],item[1],float(item[2]),int(item[3])
+
         if rating < score_thr:
             continue
         if itemid not in item_cate:
             continue
+
         time_score = get_time_score(timestamp)
         if userid not in record:
             record[userid] = {}
         for fix_cate in item_cate[itemid]:
+            #首先将每一个用户的初始化类别的得分都初始化成0分
             if fix_cate not in record[userid]:
                 record[userid][fix_cate]=0
+            #总分为用户对每个类别的喜好程度 = 得分*时间得分*该item下的权重
             record[userid][fix_cate] +=rating*time_score*item_cate[itemid][fix_cate]
     fp.close()
+    #召回完毕，之后进行排序
     for userid in record:
         if userid not in up:
             up[userid]=[]
-        total_score =0
+        total_score =0 #这里要对类别得分做归一化处理
+        #给用户进行排序，并取两个最受欢迎的类别
+        # record数据结构{userid:[{类别1：总分},{类别2：总分}]}
         for zuhe in sorted(record[userid].items(),key=operator.itemgetter(1),reverse=True)[:topk]:
             up[userid].append((zuhe[0],zuhe[1]))
             total_score +=zuhe[1]
         for index in range(len(up[userid])) :
+            # 将最后总得分进行归一化
             up[userid][index] =(up[userid][index][0],round(up[userid][index][1]/total_score,3))
+    return up
+
 def get_time_score(timestamp):
     """
     :param timestamp:输入时间戳
     :return:time score
     """
-    fix_time_stamp = 1476086345
+    fix_time_stamp = 1476086345 #用户评分文件里面最大的时间戳
     total_sec = 24*60*60
-    delta = (fix_time_stamp-timestamp)/total_sec/100        #秒级时间戳应该转化成天级
-    return round(1/(1+delta),3)                         #差距越大得分越低
-
+    delta = (fix_time_stamp-timestamp)/total_sec/100   #秒级时间戳应该转化成天级
+    return round(1/(1+delta),3)                   #差距越小得分越高差距越大得分越低
+# 排序过程
 def recom(cate_item_sort,up,userid,topk =10):
     """
     :param cate_item_sort:
@@ -79,12 +90,15 @@ def recom(cate_item_sort,up,userid,topk =10):
     recom_result = {}
     if userid not in recom_result:
         recom_result[userid]=[]
+    #如果用户在用户互相当中，对每一个类别进行召回
     for zuhe in up[userid]:
         cate = zuhe[0]
-        ratio = zuhe[1]
+        ratio = zuhe[1] #权重
         num  = int(topk*ratio)+1
+        # 如果再倒排中找不到这个种类直接过
         if cate not in cate_item_sort:
             continue
+        # 可以找到就完成推荐
         recom_list = cate_item_sort[cate][:num]
         recom_result[userid] += recom_list
     return recom_result
@@ -93,6 +107,6 @@ def run_main():
     ave_score=read.get_ave_score("../data/ratings15000.csv")
     item_cate,cate_item_sort=read.get_item_cate(ave_score,"../data/movies.csv")
     up = get_up(item_cate,"../data/ratings15000.csv")
-    # print(recom(cate_item_sort,up,'1'))
+    print(recom(cate_item_sort,up,'1'))
 if __name__ == '__main__':
     run_main()
--- util/read.py
+++ util/read.py
@@ -11,39 +11,40 @@ util function
 from __future__ import division
 import os
 import operator
-import sys
 
 
+#获取平均分的函数方法
 def get_ave_score(input_file):
     if not os.path.exists(input_file):
         return {}
     linenum = 0
-    record_dict = {}
-    score_dict = {}
+    record_dict = {} #计算评分的数据结构
+    score_dict = {} #要返回的数据结构
     fp = open(input_file)
     for line in fp:
         if linenum == 0:
             linenum += 1
             continue
         item = line.strip().split(',')
-        if len(item)<4:
+        if len(item)<4:#如果列数小于4就过滤掉
             continue
         userid,itemid,rating = item[0],item[1],float(item[2])
         if itemid not in record_dict:
             record_dict[itemid]=[0,0]
-        record_dict[itemid][0] += 1
-        record_dict[itemid][1] += rating
-    fp.closed
+        record_dict[itemid][0] += 1 #有多少个人评分
+        record_dict[itemid][1] += rating #总分是多少
+    fp.close()
     for itemid in record_dict:
         score_dict[itemid] = round(record_dict[itemid][1]/record_dict[itemid][0],3)
     return score_dict
 
+# 第一个返回值从item对应类别的字典 字典里面套字典，每个类别对应的分数，第二个返回值是类别对应的item的倒排表
 def get_item_cate(ave_score,input_file):
     """
     :param ave_score: a dict ,key itemid value rating score
     :param input_file: item info file
-    :return:a dict:key itemid value a dict,key:cate value:ratio 每个对应的类别
-            a dict:key cate value [itemid1,item2,itemid3] 每个类别对应的item的倒排
+    :return:a dict:key itemid value a dict,key:cate value:ratio 每个itemid对应的类别
+            a dict:key cate value [itemid1,item2,itemid3] 每个类别对应的itemid的倒排表
     """
     if not os.path.exists(input_file):
         return {},{}
@@ -60,14 +61,16 @@ def get_item_cate(ave_score,input_file):
         if len(item)<3:
             continue
         itemid = item[0]
-        cate_str = item[-1]
-        cate_list = cate_str.strip().split("|")
-        ratio = round(1/len(cate_list),3)
+        cate_str = item[-1]# 取出类别
+        cate_list = cate_str.strip().split("|")#类别划成数组
+        ratio = round(1/len(cate_list),3)#不同类别均分权重
         if itemid not in item_cate:
             item_cate[itemid]={}
         for fix_cate in cate_list:
-            item_cate[itemid][fix_cate]=ratio
+            item_cate[itemid][fix_cate]=ratio#把权重定义给itemid的每个类别
     fp.close()
+    #获得每个种类下的item的倒排
+    #记录每一个种类下item的平均的评分
     for itemid in item_cate:
         for cate in item_cate[itemid]:
             if cate not in record:
@@ -78,19 +81,22 @@ def get_item_cate(ave_score,input_file):
         if cate not in cate_item_sort:
             cate_item_sort[cate]=[]
         for zuhe in sorted(record[cate].items(),key=operator.itemgetter(1),reverse=True)[:100]:
-            cate_item_sort[cate].append(zuhe[0]+"-"+str(zuhe[1]))
+            # .items()方法将字典中的无序的键值对转成了有序的元组，每一个zuhe都是一个元组
+            # zuhe[0]是元组中的第一项也就是itemid，zuhe[1]是元组中第二项也就是分数
+            # cate_item_sort[cate].append(zuhe[0]+"-"+str(zuhe[1]))
+            cate_item_sort[cate].append(zuhe[0])
     return item_cate, cate_item_sort
 
 if __name__ == '__main__':
 
     ave_score = get_ave_score('../data/ratings15000.csv')
     # print(len(ave_score))
-    # print(ave_score['32'])
+    # print(ave_score['34'])
     item_cate,cate_item_sort = get_item_cate(ave_score,'../data/movies.csv')
     # print(len(item_cate))
     # print(item_cate['1'])
     # print(cate_item_sort['Children'])
-    # t rain_data = get_train_data('../data/ratings.csv')
+    # t rain_data =  get_train_data('../data/ratings.csv')
     # print train_data[:20]
     # print(len(train_data))
 
